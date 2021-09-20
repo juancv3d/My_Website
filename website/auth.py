8240b675346153e3eb3047ad91ebd8203fee74f9
juancv3d <juancvillarios@hotmail.com>
2021-09-19 23:23:07 -0500

    Simple auth statments

--- website/auth.py
+++ website/auth.py
@@ -1,11 +1,14 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+from flask.globals import request
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return '<p>Login</p>'
+    data = request.form
+    print(data)
+    return render_template('login.html')
 
 
 @auth.route('/logout')
@@ -13,6 +16,22 @@ def logout():
     return '<p>Logout</p>'
 
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
-    return '<p>Sign Up</p>'
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        password_confirm = request.form.get('password_confirm')
+
+        if len(email) < 4:
+            flash('Email is too short. Must be grater than 4 characters.',
+                  category='error')
+        elif len(password) < 7:
+            flash(
+                'Password is too short. Must be grater than 7 characters.', category='error')
+        elif password != password_confirm:
+            flash('Passwords do not match.', category='error')
+        else:
+            flash('User created successfully.', category='success')
+
+    return render_template('sign_up.html')
